5162756ccd7f7ec6a148d1004637bf73a7c9596e
Lisa Bureau <bureau.lisa03@gmail.com>
2026-07-09 14:34:30 +0200

    Installing the PrimeNG package for the homepage carousel

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,20 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeng/themes/lara';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Lara,
+        }
+    }),
     { provide: LOCALE_ID, useValue: 'fr-FR'}
   ]
 };
