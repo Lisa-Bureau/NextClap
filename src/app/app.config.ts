33ae108b5ecb5e9356f256dd7e7b97c0763aee09
Lisa Bureau <bureau.lisa03@gmail.com>
2026-07-17 15:01:28 +0200

    Added `provideAppInitializer` to initialize the navigation service as soon as the app starts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,17 @@
-import { ApplicationConfig, LOCALE_ID, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, LOCALE_ID, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavigationService } from './services/navigation.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideAnimationsAsync(),
+    provideAppInitializer(() => { inject(NavigationService) }),
     providePrimeNG({
         theme: {
             preset: Lara,
