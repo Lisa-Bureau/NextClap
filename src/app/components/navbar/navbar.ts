62c5abbe5f65e295500bc3156141ef17f64e2a41
Lisa Bureau <bureau.lisa03@gmail.com>
2026-07-21 16:53:02 +0200

    Close the burger menu when clicked outside of it

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { SearchBar } from '../search-bar/search-bar';
 
@@ -12,8 +12,25 @@ export class Navbar {
 
   menuIsOpen: boolean = false;
 
+  constructor(private elementRef: ElementRef) {}
+
   /* Alterne l'état d'ouverture du menu burger. */
   toggleMenu(): void {
     this.menuIsOpen = !this.menuIsOpen;
+  }
+
+  /**
+   * Écouteur global sur le document. 
+   * Déclenché à chaque clic n'importe où sur l'écran.
+   */
+  @HostListener("document:click", ["$event"])
+  clickOut(event: MouseEvent) {
+    // Vérifie si l'élément cliqué (event.target) se trouve physiquement à l'intérieur de ce composant
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+
+    // Si le clic a eu lieu à l'extérieur du composant, on ferme automatiquement le dropdown
+    if (!clickedInside) {
+      this.menuIsOpen = false;
+    }
   }
 }
